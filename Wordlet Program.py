d428eea3bd7f5a2bee98230069eca4c7f7a21ec1
TylerSafe <98946533+TylerSafe@users.noreply.github.com>
2022-02-24 17:09:48 +0800

    Add files via upload
    - Added dictionary text file
    - Added start and reset button
    - Added all required game buttons
    - First word is now found and inserted into the first line when game is started

--- Wordlet Program.py
+++ Wordlet Program.py
@@ -1,4 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 
 class Ui_MainWindow(object):
@@ -8,31 +11,159 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(310, 30, 141, 41))
+        self.label.setGeometry(QtCore.QRect(310, 20, 141, 41))
         self.label.setObjectName("label")
         self.first_1 = QtWidgets.QPushButton(self.centralwidget)
-        self.first_1.setGeometry(QtCore.QRect(200, 100, 51, 51))
+        self.first_1.setGeometry(QtCore.QRect(200, 120, 51, 51))
         self.first_1.setText("")
         self.first_1.setObjectName("first_1")
+        self.first_1.setFont(QFont('Times', 20))
         self.first_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.first_2.setGeometry(QtCore.QRect(270, 100, 51, 51))
+        self.first_2.setGeometry(QtCore.QRect(270, 120, 51, 51))
         self.first_2.setText("")
         self.first_2.setObjectName("first_2")
+        self.first_2.setFont(QFont('Times', 20))
         self.first_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.first_3.setGeometry(QtCore.QRect(340, 100, 51, 51))
+        self.first_3.setGeometry(QtCore.QRect(340, 120, 51, 51))
         self.first_3.setText("")
         self.first_3.setObjectName("first_3")
+        self.first_3.setFont(QFont('Times', 20))
         self.first_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.first_4.setGeometry(QtCore.QRect(410, 100, 51, 51))
+        self.first_4.setGeometry(QtCore.QRect(410, 120, 51, 51))
         self.first_4.setText("")
         self.first_4.setObjectName("first_4")
+        self.first_4.setFont(QFont('Times', 20))
         self.first_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.first_5.setGeometry(QtCore.QRect(480, 100, 51, 51))
+        self.first_5.setGeometry(QtCore.QRect(480, 120, 51, 51))
         self.first_5.setText("")
         self.first_5.setObjectName("first_5")
+        self.first_5.setFont(QFont('Times', 20))
         self.submit_1 = QtWidgets.QPushButton(self.centralwidget)
-        self.submit_1.setGeometry(QtCore.QRect(560, 110, 81, 31))
+        self.submit_1.setGeometry(QtCore.QRect(560, 130, 81, 31))
         self.submit_1.setObjectName("submit_1")
+        self.start = QtWidgets.QPushButton(self.centralwidget)
+        self.start.setGeometry(QtCore.QRect(340, 70, 81, 31))
+        self.start.setObjectName("start")
+        self.second_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.second_1.setGeometry(QtCore.QRect(200, 190, 51, 51))
+        self.second_1.setText("")
+        self.second_1.setObjectName("second_1")
+        self.second_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.second_3.setGeometry(QtCore.QRect(340, 190, 51, 51))
+        self.second_3.setText("")
+        self.second_3.setObjectName("second_3")
+        self.submit_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.submit_2.setGeometry(QtCore.QRect(560, 200, 81, 31))
+        self.submit_2.setObjectName("submit_2")
+        self.second_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.second_2.setGeometry(QtCore.QRect(270, 190, 51, 51))
+        self.second_2.setText("")
+        self.second_2.setObjectName("second_2")
+        self.second_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.second_4.setGeometry(QtCore.QRect(410, 190, 51, 51))
+        self.second_4.setText("")
+        self.second_4.setObjectName("second_4")
+        self.second_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.second_5.setGeometry(QtCore.QRect(480, 190, 51, 51))
+        self.second_5.setText("")
+        self.second_5.setObjectName("second_5")
+        self.third_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.third_1.setGeometry(QtCore.QRect(200, 260, 51, 51))
+        self.third_1.setText("")
+        self.third_1.setObjectName("third_1")
+        self.third_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.third_3.setGeometry(QtCore.QRect(340, 260, 51, 51))
+        self.third_3.setText("")
+        self.third_3.setObjectName("third_3")
+        self.submit_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.submit_3.setGeometry(QtCore.QRect(560, 270, 81, 31))
+        self.submit_3.setObjectName("submit_3")
+        self.third_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.third_2.setGeometry(QtCore.QRect(270, 260, 51, 51))
+        self.third_2.setText("")
+        self.third_2.setObjectName("third_2")
+        self.third_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.third_4.setGeometry(QtCore.QRect(410, 260, 51, 51))
+        self.third_4.setText("")
+        self.third_4.setObjectName("third_4")
+        self.third_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.third_5.setGeometry(QtCore.QRect(480, 260, 51, 51))
+        self.third_5.setText("")
+        self.third_5.setObjectName("third_5")
+        self.fourth_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.fourth_1.setGeometry(QtCore.QRect(200, 330, 51, 51))
+        self.fourth_1.setText("")
+        self.fourth_1.setObjectName("fourth_1")
+        self.fourth_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.fourth_3.setGeometry(QtCore.QRect(340, 330, 51, 51))
+        self.fourth_3.setText("")
+        self.fourth_3.setObjectName("fourth_3")
+        self.submit_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.submit_4.setGeometry(QtCore.QRect(560, 340, 81, 31))
+        self.submit_4.setObjectName("submit_4")
+        self.fourth_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.fourth_2.setGeometry(QtCore.QRect(270, 330, 51, 51))
+        self.fourth_2.setText("")
+        self.fourth_2.setObjectName("fourth_2")
+        self.fourth_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.fourth_4.setGeometry(QtCore.QRect(410, 330, 51, 51))
+        self.fourth_4.setText("")
+        self.fourth_4.setObjectName("fourth_4")
+        self.fourth_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.fourth_5.setGeometry(QtCore.QRect(480, 330, 51, 51))
+        self.fourth_5.setText("")
+        self.fourth_5.setObjectName("fourth_5")
+        self.fifth_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.fifth_1.setGeometry(QtCore.QRect(200, 400, 51, 51))
+        self.fifth_1.setText("")
+        self.fifth_1.setObjectName("fifth_1")
+        self.fifth_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.fifth_3.setGeometry(QtCore.QRect(340, 400, 51, 51))
+        self.fifth_3.setText("")
+        self.fifth_3.setObjectName("fifth_3")
+        self.submit_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.submit_5.setGeometry(QtCore.QRect(560, 410, 81, 31))
+        self.submit_5.setObjectName("submit_5")
+        self.fifth_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.fifth_2.setGeometry(QtCore.QRect(270, 400, 51, 51))
+        self.fifth_2.setText("")
+        self.fifth_2.setObjectName("fifth_2")
+        self.fifth_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.fifth_4.setGeometry(QtCore.QRect(410, 400, 51, 51))
+        self.fifth_4.setText("")
+        self.fifth_4.setObjectName("fifth_4")
+        self.fifth_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.fifth_5.setGeometry(QtCore.QRect(480, 400, 51, 51))
+        self.fifth_5.setText("")
+        self.fifth_5.setObjectName("fifth_5")
+        self.sixth_1 = QtWidgets.QPushButton(self.centralwidget)
+        self.sixth_1.setGeometry(QtCore.QRect(200, 470, 51, 51))
+        self.sixth_1.setText("")
+        self.sixth_1.setObjectName("sixth_1")
+        self.sixth_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.sixth_3.setGeometry(QtCore.QRect(340, 470, 51, 51))
+        self.sixth_3.setText("")
+        self.sixth_3.setObjectName("sixth_3")
+        self.submit_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.submit_6.setGeometry(QtCore.QRect(560, 480, 81, 31))
+        self.submit_6.setObjectName("submit_6")
+        self.sixth_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.sixth_2.setGeometry(QtCore.QRect(270, 470, 51, 51))
+        self.sixth_2.setText("")
+        self.sixth_2.setObjectName("sixth_2")
+        self.sixth_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.sixth_4.setGeometry(QtCore.QRect(410, 470, 51, 51))
+        self.sixth_4.setText("")
+        self.sixth_4.setObjectName("sixth_4")
+        self.sixth_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.sixth_5.setGeometry(QtCore.QRect(480, 470, 51, 51))
+        self.sixth_5.setText("")
+        self.sixth_5.setObjectName("sixth_5")
+        self.reset_game = QtWidgets.QPushButton(self.centralwidget)
+        self.reset_game.setGeometry(QtCore.QRect(560, 130, 81, 31))
+        self.reset_game.setText("")
+        self.reset_game.setObjectName("reset_game")
+        self.reset_game.move(340, 538)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
@@ -41,40 +172,80 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
-        self.counter = ButtonClicks()
+       
+        # get all the word data from the text file
+        f = open('words.txt', 'r')
+        self.words = f.read().split()
+        # create list to track amount of button clicks
+        self.count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  
         
-        self.submit_1.clicked.connect(lambda: self.calculate_word())
+        # when a word is submitted perform the calculation
+        self.start.clicked.connect(lambda: self.calculate_word(0))
+        self.submit_1.clicked.connect(lambda: self.calculate_word(1))
+        self.submit_1.clicked.connect(lambda: self.calculate_word(2))
+        self.submit_1.clicked.connect(lambda: self.calculate_word(3))
+        self.submit_1.clicked.connect(lambda: self.calculate_word(4))
+        self.submit_1.clicked.connect(lambda: self.calculate_word(5))
+        self.submit_1.clicked.connect(lambda: self.calculate_word(6))
         
+        # chance the colour of buttons as they are selected to idicate whether they are in the word or correct position
         self.first_1.clicked.connect(lambda: self.button_colour(self.first_1, 0))
         self.first_2.clicked.connect(lambda: self.button_colour(self.first_2, 1))
         self.first_3.clicked.connect(lambda: self.button_colour(self.first_3, 2))
         self.first_4.clicked.connect(lambda: self.button_colour(self.first_4, 3))
         self.first_5.clicked.connect(lambda: self.button_colour(self.first_5, 4))
+        self.second_1.clicked.connect(lambda: self.button_colour(self.second_1, 5))
+        self.second_2.clicked.connect(lambda: self.button_colour(self.second_2, 6))
+        self.second_3.clicked.connect(lambda: self.button_colour(self.second_3, 7))
+        self.second_4.clicked.connect(lambda: self.button_colour(self.second_4, 8))
+        self.second_5.clicked.connect(lambda: self.button_colour(self.second_5, 9))
+        self.third_1.clicked.connect(lambda: self.button_colour(self.third_1, 10))
+        self.third_2.clicked.connect(lambda: self.button_colour(self.third_2, 11))
+        self.third_3.clicked.connect(lambda: self.button_colour(self.third_3, 12))
+        self.third_4.clicked.connect(lambda: self.button_colour(self.third_4, 13))
+        self.third_5.clicked.connect(lambda: self.button_colour(self.third_5, 14))
+        self.fourth_1.clicked.connect(lambda: self.button_colour(self.fourth_1, 15))
+        self.fourth_2.clicked.connect(lambda: self.button_colour(self.fourth_2, 16))
+        self.fourth_3.clicked.connect(lambda: self.button_colour(self.fourth_3, 17))
+        self.fourth_4.clicked.connect(lambda: self.button_colour(self.fourth_4, 18))
+        self.fourth_5.clicked.connect(lambda: self.button_colour(self.fourth_5, 19))
+        self.fifth_1.clicked.connect(lambda: self.button_colour(self.fifth_1, 20))
+        self.fifth_2.clicked.connect(lambda: self.button_colour(self.fifth_2, 21))
+        self.fifth_3.clicked.connect(lambda: self.button_colour(self.fifth_3, 22))
+        self.fifth_4.clicked.connect(lambda: self.button_colour(self.fifth_4, 23))
+        self.fifth_5.clicked.connect(lambda: self.button_colour(self.fifth_5, 24))
+        self.sixth_1.clicked.connect(lambda: self.button_colour(self.sixth_1, 25))
+        self.sixth_2.clicked.connect(lambda: self.button_colour(self.sixth_2, 26))
+        self.sixth_3.clicked.connect(lambda: self.button_colour(self.sixth_3, 27))
+        self.sixth_4.clicked.connect(lambda: self.button_colour(self.sixth_4, 28))
+        self.sixth_5.clicked.connect(lambda: self.button_colour(self.sixth_5, 29))
 
+        # reset the game
+        self.reset_game.clicked.connect(lambda: self.reset())
+        
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
     
     def button_colour(self, button, number):
+        # check how many times the button has been clicked
+        self.count[number] += 1        
+        count = self.count[number]
 
-        count = self.counter.button_counter(number)
-
+        # based on the number of clicks cycle through the different colours
         if (count - 2) % 3 == 0:
             button.setStyleSheet("QPushButton""{""background-color : #ffd24d;""}")
         elif count % 3 == 0:
             button.setStyleSheet("QPushButton""{""background-color : None;""}")
         else:
             button.setStyleSheet("QPushButton""{""background-color : #44a670;""}")
-
-    def calculate_word(self):
-        
+    
+    def calculate_word(self, progress):
+        # declare variables
         min_word = ''
         min_score = 99
 
-        f = open('words.txt', 'r')
-        words = f.read().split()
-
-        for word in words:
+        # calculate the word score
+        for word in self.words:
             score = 0
             for letter in word:
                 if letter == 'a':
@@ -132,30 +303,38 @@ class Ui_MainWindow(object):
                 else:
                     print('Error! Word contains non standard character')
 
+            # if the new word has a lower score replace it
             if score <= min_score:
                 min_score = score
-                min_word = word
-        print('word:', min_word, '   score:', min_score)
-
-        return min_word
+                min_word = word.upper()
         
+        if progress == 0:
+            self.first_1.setText(min_word[0])
+            self.first_2.setText(min_word[1])
+            self.first_3.setText(min_word[2])
+            self.first_4.setText(min_word[3])
+            self.first_5.setText(min_word[4])
+
+    def reset(self):
+        self.count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.first_1.setText('')
+        self.first_2.setText('')
+        self.first_3.setText('')
+        self.first_4.setText('')
+        self.first_5.setText('')
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:16pt;\">Wordlet Solver</span></p></body></html>"))
         self.submit_1.setText(_translate("MainWindow", "Submit"))
-
-
-class ButtonClicks:
-    def __init__(self):
-        # class variables
-        self.count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]    
-    
-    def button_counter(self, number):            
-        self.count[number] += 1
-        
-        return self.count[number]
+        self.start.setText(_translate("MainWindow", "Start"))
+        self.submit_2.setText(_translate("MainWindow", "Submit"))
+        self.submit_3.setText(_translate("MainWindow", "Submit"))
+        self.submit_4.setText(_translate("MainWindow", "Submit"))
+        self.submit_5.setText(_translate("MainWindow", "Submit"))
+        self.submit_6.setText(_translate("MainWindow", "Submit"))
+        self.reset_game.setText(_translate("MainWindow", "Reset"))
 
 
 if __name__ == "__main__":
